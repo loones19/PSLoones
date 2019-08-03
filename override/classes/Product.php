dd32cbc515a3b4752961a8f885235dc110529337
Rafael García Cuéllar <r.gc@hotmail.es>
2019-08-03 20:38:02 +0200

    Fix error
    Fixed Error product doesn't save format pack. (Bulk/Bottling).

--- override/classes/Product.php
+++ override/classes/Product.php
@@ -1,37 +1,125 @@
 <?php
 class Product extends ProductCore
 {
-	    
-		/*
-    * module: agilemultipleseller
-    * date: 2019-06-24 19:40:27
-    * version: 3.7.3.2
-    */
-    public $id_trans_op1;
-		/*
-    * module: agilemultipleseller
-    * date: 2019-06-24 19:40:27
-    * version: 3.7.3.2
-    */
-    public $id_trans_op2;
-		/*
-    * module: agilemultipleseller
-    * date: 2019-06-24 19:40:27
-    * version: 3.7.3.2
-    */
-    public $trans_op2_n1;
-		/*
-    * module: agilemultipleseller
-    * date: 2019-06-24 19:40:27
-    * version: 3.7.3.2
-    */
+	/** @var string Transporte */
+	public $id_trans_op1;
+	public $id_trans_op2;
+	public $trans_op2_n1;
     public $trans_op2_n2;
-		
-	/*
-    * module: agilemultipleseller
-    * date: 2019-06-24 19:40:27
+        
+    /*
+	* module: agilemultipleseller
+    * date: 2019-02-22 17:35:37
     * version: 3.7.3.2
-    */
+	*/
+	public $id_tax_rules_group = 2;   //LOONES
+	public $trans_op2_n3;
+	public $id_tipo;
+	
+	public static $definition = array(
+        'table' => 'product',
+        'primary' => 'id_product',
+        'multilang' => true,
+        'multilang_shop' => true,
+        'fields' => array(
+            'id_shop_default' => array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+            'id_manufacturer' => array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+            'id_supplier' => array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+            'reference' => array('type' => self::TYPE_STRING, 'validate' => 'isReference', 'size' => 64),
+            'supplier_reference' => array('type' => self::TYPE_STRING, 'validate' => 'isReference', 'size' => 64),
+            'location' => array('type' => self::TYPE_STRING, 'validate' => 'isReference', 'size' => 64),
+            'width' => array('type' => self::TYPE_FLOAT, 'validate' => 'isUnsignedFloat'),
+            'height' => array('type' => self::TYPE_FLOAT, 'validate' => 'isUnsignedFloat'),
+            'depth' => array('type' => self::TYPE_FLOAT, 'validate' => 'isUnsignedFloat'),
+			'weight' => array('type' => self::TYPE_FLOAT, 'validate' => 'isUnsignedFloat'),
+			'id_trans_op1' 	=> array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+			'id_trans_op2' 	=> array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+			'trans_op2_n1' 	=> array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+			'trans_op2_n2' 	=> array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+			'trans_op2_n3' 	=> array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+			'id_tipo' 		=> array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+            'quantity_discount' => array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+            'ean13' => array('type' => self::TYPE_STRING, 'validate' => 'isEan13', 'size' => 13),
+            'isbn' => array('type' => self::TYPE_STRING, 'validate' => 'isIsbn', 'size' => 32),
+            'upc' => array('type' => self::TYPE_STRING, 'validate' => 'isUpc', 'size' => 12),
+            'cache_is_pack' => array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+            'cache_has_attachments' => array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+            'is_virtual' => array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
+            'state' => array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+            'additional_delivery_times' => array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
+            'delivery_in_stock' => array(
+                'type' => self::TYPE_STRING,
+                'lang' => true,
+                'validate' => 'isGenericName',
+                'size' => 255,
+            ),
+            'delivery_out_stock' => array(
+                'type' => self::TYPE_STRING,
+                'lang' => true,
+                'validate' => 'isGenericName',
+                'size' => 255,
+            ),
+            'id_category_default' => array('type' => self::TYPE_INT, 'shop' => true, 'validate' => 'isUnsignedId'),
+            'id_tax_rules_group' => array('type' => self::TYPE_INT, 'shop' => true, 'validate' => 'isUnsignedId'),
+            'on_sale' => array('type' => self::TYPE_BOOL, 'shop' => true, 'validate' => 'isBool'),
+            'online_only' => array('type' => self::TYPE_BOOL, 'shop' => true, 'validate' => 'isBool'),
+            'ecotax' => array('type' => self::TYPE_FLOAT, 'shop' => true, 'validate' => 'isPrice'),
+            'minimal_quantity' => array('type' => self::TYPE_INT, 'shop' => true, 'validate' => 'isUnsignedInt'),
+            'low_stock_threshold' => array('type' => self::TYPE_INT, 'shop' => true, 'allow_null' => true, 'validate' => 'isInt'),
+            'low_stock_alert' => array('type' => self::TYPE_BOOL, 'shop' => true, 'allow_null' => true, 'validate' => 'isBool'),
+            'price' => array('type' => self::TYPE_FLOAT, 'shop' => true, 'validate' => 'isPrice', 'required' => true),
+            'wholesale_price' => array('type' => self::TYPE_FLOAT, 'shop' => true, 'validate' => 'isPrice'),
+            'unity' => array('type' => self::TYPE_STRING, 'shop' => true, 'validate' => 'isString'),
+            'unit_price_ratio' => array('type' => self::TYPE_FLOAT, 'shop' => true),
+            'additional_shipping_cost' => array('type' => self::TYPE_FLOAT, 'shop' => true, 'validate' => 'isPrice'),
+            'customizable' => array('type' => self::TYPE_INT, 'shop' => true, 'validate' => 'isUnsignedInt'),
+            'text_fields' => array('type' => self::TYPE_INT, 'shop' => true, 'validate' => 'isUnsignedInt'),
+            'uploadable_files' => array('type' => self::TYPE_INT, 'shop' => true, 'validate' => 'isUnsignedInt'),
+            'active' => array('type' => self::TYPE_BOOL, 'shop' => true, 'validate' => 'isBool'),
+            'redirect_type' => array('type' => self::TYPE_STRING, 'shop' => true, 'validate' => 'isString'),
+            'id_type_redirected' => array('type' => self::TYPE_INT, 'shop' => true, 'validate' => 'isUnsignedId'),
+            'available_for_order' => array('type' => self::TYPE_BOOL, 'shop' => true, 'validate' => 'isBool'),
+            'available_date' => array('type' => self::TYPE_DATE, 'shop' => true, 'validate' => 'isDateFormat'),
+            'show_condition' => array('type' => self::TYPE_BOOL, 'shop' => true, 'validate' => 'isBool'),
+            'condition' => array('type' => self::TYPE_STRING, 'shop' => true, 'validate' => 'isGenericName', 'values' => array('new', 'used', 'refurbished'), 'default' => 'new'),
+            'show_price' => array('type' => self::TYPE_BOOL, 'shop' => true, 'validate' => 'isBool'),
+            'indexed' => array('type' => self::TYPE_BOOL, 'shop' => true, 'validate' => 'isBool'),
+            'visibility' => array('type' => self::TYPE_STRING, 'shop' => true, 'validate' => 'isProductVisibility', 'values' => array('both', 'catalog', 'search', 'none'), 'default' => 'both'),
+            'cache_default_attribute' => array('type' => self::TYPE_INT, 'shop' => true),
+            'advanced_stock_management' => array('type' => self::TYPE_BOOL, 'shop' => true, 'validate' => 'isBool'),
+            'date_add' => array('type' => self::TYPE_DATE, 'shop' => true, 'validate' => 'isDate'),
+            'date_upd' => array('type' => self::TYPE_DATE, 'shop' => true, 'validate' => 'isDate'),
+            'pack_stock_type' => array('type' => self::TYPE_INT, 'shop' => true, 'validate' => 'isUnsignedInt'),
+            'meta_description' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName', 'size' => 512),
+            'meta_keywords' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName', 'size' => 255),
+            'meta_title' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName', 'size' => 255),
+            'link_rewrite' => array(
+                'type' => self::TYPE_STRING,
+                'lang' => true,
+                'validate' => 'isLinkRewrite',
+                'required' => false,
+                'size' => 128,
+                'ws_modifier' => array(
+                    'http_method' => WebserviceRequest::HTTP_POST,
+                    'modifier' => 'modifierWsLinkRewrite',
+                ),
+            ),
+            'name' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isCatalogName', 'required' => false, 'size' => 128),
+            'description' => array('type' => self::TYPE_HTML, 'lang' => true, 'validate' => 'isCleanHtml'),
+            'description_short' => array('type' => self::TYPE_HTML, 'lang' => true, 'validate' => 'isCleanHtml'),
+            'available_now' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName', 'size' => 255),
+            'available_later' => array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'IsGenericName', 'size' => 255),
+        ),
+        'associations' => array(
+            'manufacturer' => array('type' => self::HAS_ONE),
+            'supplier' => array('type' => self::HAS_ONE),
+            'default_category' => array('type' => self::HAS_ONE, 'field' => 'id_category_default', 'object' => 'Category'),
+            'tax_rules_group' => array('type' => self::HAS_ONE),
+            'categories' => array('type' => self::HAS_MANY, 'field' => 'id_category', 'object' => 'Category', 'association' => 'category_product'),
+            'stock_availables' => array('type' => self::HAS_MANY, 'field' => 'id_stock_available', 'object' => 'StockAvailable', 'association' => 'stock_availables'),
+        ),
+	);
+	
     public function setCarriers($carrier_list)
 	{
 		if(Module::isInstalled('agilesellershipping') AND !empty($carrier_list))
